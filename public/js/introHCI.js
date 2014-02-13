e9added0edd7cad78c26b850aed26b8d676937db
vagrant <vagrant@precise32.(none)>
2014-02-13 02:38:08 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,17 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	var url = '/project/'+idNumber;
+	console.log('URL: ' + url);
+	$.get(url,appendProjectDetails);
+}
+
+function appendProjectDetails(result) {
+	console.log(result);
+	var html = '<h4>'+result.date+'</h4>';
+	html += '<img src="'+result.image+'" class="detailsImage"/>';
+	html += result.summary;
+	$('#project'+result.id+' .details').html(html);;
 }
 
 /*
@@ -35,4 +46,14 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get('/palette',changePalette);	
+}
+
+function changePalette(result) {
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
